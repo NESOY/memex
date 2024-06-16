55d143a28bdfc7d8102d21782747a45824782634
youngjae kwon <kyoje11@gmail.com>
2024-06-16 23:29:30 +0900

    update favicon

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      // { provider: 'google', tagId: '<your-google-tag>' }
+      // { provider: 'google', tagId: '89701804' }
       provider: "plausible",
     },
     locale: "en-US",
