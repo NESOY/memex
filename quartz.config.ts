8d8fbd0843799bbb59be627e6a58efccb2496546
youngjae kwon <kyoje11@gmail.com>
2024-06-17 00:02:37 +0900

    update font

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Poppins",
+        body: "Noto Sans Korean",
         code: "IBM Plex Mono",
       },
       colors: {
