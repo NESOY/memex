c42e28d9201405ee71ae4c022b41adc9cbb16946
youngjae kwon <kyoje11@gmail.com>
2024-06-18 22:56:19 +0900

    Revert "Update Google Adsense"
    This reverts commit 1a9ca21eb92a00b9112c840fffe4d142aa3ff280.
    
    Revert "Update Google Adsense"
    
    This reverts commit 1651572a6ce74549ab806ccad14386e23f65b7f0.

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -5,20 +5,7 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 export default ((opts?: Options) => {
   const Comments: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     return (
-      
       <footer class={`${displayClass ?? "comments"}`}>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-1829817529831781"
-          data-ad-slot="7543380157"
-          data-ad-format="auto"
-          data-full-width-responsive="false"
-          ></ins>
-          
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
         <hr />
         <div class="giscus">
         </div>
